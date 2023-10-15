734a3f740337ab00ca2789d6e21f1bf14dcd2d25
Vicente Rivera <vicenterivera225@gmail.com>
2023-10-15 17:33:25 -0300

    avance+

---
+++ script/contact.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("msg-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById("msg-name").value;
+  const email = document.getElementById("msg-email").value;
+  const mensaje = document.getElementById("msg-msg").value;
+  mostrarDatos(nombre, email, mensaje);
+});
+
+function mostrarDatos(nombre, email, mensaje) {
+  alert(
+    `Mensaje enviado!\n Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`
+  );
+}
---
+++ script/index.js
@@ -0,0 +1,44 @@
+
+    const modal = document.getElementById('infoModal');
+    const modalTitle = document.getElementById('modalTitle');
+    const modalDescription = document.getElementById('modalDescription');
+    const modalLink = document.getElementById('modalLink');
+    const closeModal = document.getElementById('closeModal');
+
+    function mostrarModal(titulo, descripcion, enlace) {
+        modalTitle.textContent = titulo;
+        modalDescription.textContent = descripcion;
+        modalLink.href = enlace;
+        modal.classList.remove('hidden');
+    }
+
+    document.getElementById('op-card').addEventListener('click', function() {
+        modal.classList.add('flex');
+        mostrarModal(
+            'One Piece', 
+            'Descripción de One Piece...', 
+            'pages/games/OnePiece.html'
+        );
+    });
+
+    document.getElementById('ao-card').addEventListener('click', function() {
+        modal.classList.add('flex');
+        mostrarModal(
+            'Ao Ashi', 
+            'Descripción de Ao Ashi...', 
+            'pages/games/AoAshi.html'
+        );
+    });
+
+    document.getElementById('sl-card').addEventListener('click', function() {
+        modal.classList.add('flex');
+        mostrarModal(
+            'Solo Leveling', 
+            'Descripción de Solo Leveling...', 
+            '/ruta-a-pagina-de-solo-leveling'
+        );
+    });
+
+    closeModal.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
